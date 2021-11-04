21c21c8f768c997cfe90c775a84c9b01ed0b79ac
Linda LW <lindalw03@gmail.com>
2021-11-04 00:42:23 -0400

    Created random password generator

---
+++ Develop/script.js
@@ -0,0 +1,73 @@
+// Assignment Code
+var generateBtn = document.querySelector('#generate');
+
+// Build a string with the types of characters that the user requests
+function getCharacterSet() {
+	var upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	var lowerCaseChars = 'abcdefghijklmnopqrstuvxyz';
+	var numberChars = '123456789';
+	var specialChars = '~`!@#$%^&*()_+-={[}]|:;<,>.?/';
+
+	var charSet = '';
+	// Ask user what kind of characters should be included. Build an array of the types of characters they request. At least one type of character must be included.
+	if (confirm('Would you like to include upper case characters?')) {
+		charSet = charSet.concat(upperCaseChars);
+	}
+	
+	if (confirm('Would you like to include lower case characters?')) {
+		charSet = charSet.concat(lowerCaseChars);
+	}
+
+	if (confirm('Would you like to include numbers?')) {
+		charSet = charSet.concat(numberChars);
+	}
+
+	if (confirm('Would you like to include special characters?')) {
+		charSet = charSet.concat(specialChars);
+	}
+// return string with all requested characters
+	return charSet;
+}
+
+// Ask user for password criteria: length of password and what types of characters should be included.  Randomly select characters to create a password to display.
+function generatePassword() {
+	var characterSet = '';
+	var builtPwd = '';
+  const minPwdLength = 8;
+  const maxPwdLength = 128;
+
+	// Ask how long pwd should be between minPwdLength and maxPwdLen characters
+	var pwdLength = prompt("Enter the length of the password you would like generated (between " + minPwdLength + "-" + maxPwdLength + " characters)");
+
+	// Validate user entered a correct number between  minPwdLength & maxPwdLength
+	while (pwdLength < minPwdLength || pwdLength > maxPwdLength) {
+		pwdLength = prompt("You entered " + pwdLength + "! Enter the length of the password you would like generated ((between " + minPwdLength + "-" + maxPwdLength + " characters)");
+	}
+
+	// Build string of possible password characters based on user input. Validate that the user chose to include at least some characters
+	characterSet = getCharacterSet();
+	while (characterSet.length === 0) {
+		alert('You must select at least one character set!');
+		characterSet = getCharacterSet();
+	}
+
+	// Randomly create a string of characters the length the user originally asked for.
+	for (let i = 0; i < pwdLength; i++) {
+		index = Math.floor(Math.random() * characterSet.length);
+    builtPwd = builtPwd + characterSet[index];
+  }
+
+	// return created password in a string to be displayed
+	return builtPwd;
+}
+
+// Write password to the #password input
+function writePassword() {
+	var password = generatePassword();
+	var passwordText = document.querySelector('#password');
+
+	passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
